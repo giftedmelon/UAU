a081d222e7eb5850fc313ccffb22a525d911c8f0
giftedmelon <lyx1231996@gmail.com>
2023-12-06 14:45:24 +0900

    １

--- script.js
+++ script.js
@@ -1,37 +1,16 @@
-let slideIndex= 0;
-let slideInterval;
+let slideIndex = 0;
 
-function showSlides(){
-    const slides = document.getElementsByClassName("slide");
-    const dots = document.getElementsByClassName("dot");
-
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].computedStyleMap.display = "none";
-        dots[i].classList.remove("active");
-    }
-
-    slideIndex++;
-    if (slideIndex > slides.length) {
-        slideIndex = 1;
-    }
-
-    slides[slideIndex  - 1].computedStyleMap.display = "block";
-    dots[slideIndex - 1].classList.add("active");
-
-    slideInterval = setTimeout(showSlides, 3000);
+function showSlides() {
+  let slides = document.getElementsByClassName("mySlides");
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 2000); // Change slide every 2 seconds
 }
 
-function changeSlide(newIndex) {
-    clearTimeout(slideInterval);
-    slideIndex = newIndex;
-    showSlides();
-}
-
-const dots = document.getElementsByClassName("dot");
-for (let i=0; i < dots.length; i++) {
-    dots[i].addEventListener("click", function() {
-        changeSlide(i + 1);
-    });
-}
-
-showSlides();
+document.addEventListener("DOMContentLoaded", showSlides);
