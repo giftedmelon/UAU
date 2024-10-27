8bc4a67dbc026a66db81a399001d76b620f00257
giftedmelon <lyx1231996@gmail.com>
2024-10-27 15:48:43 +0900

    update pdf

--- 543a55d67cb8d200f5d36ac2eb7206e61dc39ac8/out2ske_6.js
+++ 543a55d67cb8d200f5d36ac2eb7206e61dc39ac8/out2ske_6.js
@@ -45,19 +45,8 @@ document.addEventListener('DOMContentLoaded', function () {
               // { folder: 'order/kanjivg_png', fileName: row['File Name'] },
               // { folder: 'order/kanjivg_sorted_o_png', fileName: row['File Name'] },
               // { folder: 'order/kanjivg_sorted_s_png', fileName: row['File Name'] }
-              { folder: 's_2_test_2/shuffle/1/shuffle_1_out_stop_png', fileName: row['filename'] },
-              { folder: 's_2_test_2/shuffle/1/shuffle_1_stop_png', fileName: row['filename'] },
-              { folder: 's_2_test_2/origin/1/origin_1_out_stop_png', fileName: row['filename'] },
-              { folder: 's_2_test_2/origin/1/origin_1_stop_png', fileName: row['filename'] },
-              { folder: 's_2_test_2/origin/2/origin_2_stop_png', fileName: row['filename'] },
-              { folder: 's_2_test_2/origin/3/origin_3_stop_png', fileName: row['filename'] },
-              { folder: 's_2_test_2/origin/4/origin_4_stop_png', fileName: row['filename'] },
-              { folder: 's_2_test_2/origin/5/origin_5_stop_png', fileName: row['filename'] },
-              { folder: 's_2_test_2/origin/6/origin_6_stop_png', fileName: row['filename'] },
-              { folder: 's_2_test_2/origin/7/origin_7_stop_png', fileName: row['filename'] },
-              { folder: 's_2_test_2/origin/8/origin_8_stop_png', fileName: row['filename'] },
-              { folder: 's_2_test_2/origin/9/origin_9_stop_png', fileName: row['filename'] },
-              { folder: 's_2_test_2/origin/10/origin_10_stop_png', fileName: row['filename'] }
+              { folder: 'best/o_b_png', fileName: row['filename'] },
+              { folder: 'best/s_b_png', fileName: row['filename'] }
             ];
 
             await addPNGRowToContainer(pngFileNames);
